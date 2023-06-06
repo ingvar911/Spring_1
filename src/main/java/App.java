ab48aefdc8f7af541902ea094688189c6d1a9e09
ingvar911 <ingvarkazan@gmail.com>
2023-06-06 13:45:53 +0300

    Тест пройден

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat =
+                (Cat) applicationContext.getBean("myCat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("myCat");
+        System.out.println(cat==cat2);
+        System.out.println(bean==bean2);
     }
 }
